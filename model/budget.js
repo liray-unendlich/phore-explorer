7c0d42b86e549b5e804e0da0df31292f5fd55465
liray-unendlich <unendlichkeit629@gmail.com>
2018-08-03 00:14:43 +0900

    add budget proposal script

---
+++ model/budget.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+
+/**
+ * Masternode
+ *
+ * Connected masternode to the network.
+ */
+const Budget = mongoose.model('Budget', new mongoose.Schema({
+  __v: { select: false, type: Number },
+  name: { index: true, required: true, type: String },
+  url: { index: true, required: true, type: String },
+  hash: { index: true, required: true, type: String },
+  sheight: { required: true, type: Number },
+  eheight: { required: true, type: Number },
+  addr: { index: true, required: true, type: String },
+  yea: { type: Number },
+  nay: { type: Number },
+  tpay: { index: true, required: true, type: Number },
+  mpay: { index: true, required: true, type: Number }
+}, { versionKey: false }), 'budget');
+
+module.exports =  Masternode;
